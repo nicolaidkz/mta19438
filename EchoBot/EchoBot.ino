ae4bfdbaa3a77e0d214f8a638eec279f7d163c51
Nicolai <ndanie16@student.aau.dk>
2019-04-25 12:49:50 +0200

    Added code for play and record
    it plays a sound from the buzzer it then record it and it's surrounding noise then saves all that as an array which later needs to be processed with FFT and cross correlation

--- EchoBot/EchoBot.ino
+++ EchoBot/EchoBot.ino
@@ -1,9 +1,48 @@
-void setup() {
-  // put your setup code here, to run once:
+#define buzz 9
+int mic = 3;
 
+int x = 0;
+int dataArray[500];
+boolean running = true;
+
+boolean playSound = true;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(buzz, OUTPUT);
+  pinMode(mic, INPUT);
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if(playSound){
+    if (running){
+      buzzSound();
+      running = false;
+      Serial.println("Started");
+    }
+    if (!running){
+      saveRecording();
+    } 
+  }
+  else if (!playSound){
+    // calculate distance here
+  }
+}
 
+void buzzSound(){
+    tone(buzz, 1000);
+    delay(10);
+    noTone(buzz);
+}
+
+void saveRecording(){
+    dataArray[x] = analogRead(A0); // sample & save
+    Serial.println(dataArray[x]);
+    x=x+1; // set up for next location
+      if (x==500){
+        running = true;
+        playSound = false;
+        Serial.println("Done");
+      }
 }
